328101760186ba5c63e2062cf62814af544fe9c7
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-09 11:21:35 -0500

    feat: build sidebar and main navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { PATHS } from "./constants/paths.constants";
 
 import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Glampings from "./pages/Glampings";
@@ -18,13 +19,16 @@ export default function App() {
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<Navigate to={PATHS.DASHBOARD} />} />
-                    <Route path={PATHS.DASHBOARD} element={<Dashboard />} />
-                    <Route path={PATHS.BOOKINGS} element={<Bookings />} />
-                    <Route path={PATHS.CABINS} element={<Glampings />} />
-                    <Route path={PATHS.USERS} element={<Users />} />
-                    <Route path={PATHS.SETTINGS} element={<Settings />} />
-                    <Route path={PATHS.ACCOUNT} element={<Account />} />
+                    <Route element={<AppLayout />}>
+                        <Route index element={<Navigate to={PATHS.DASHBOARD} />} />
+                        <Route path={PATHS.DASHBOARD} element={<Dashboard />} />
+                        <Route path={PATHS.BOOKINGS} element={<Bookings />} />
+                        <Route path={PATHS.CABINS} element={<Glampings />} />
+                        <Route path={PATHS.USERS} element={<Users />} />
+                        <Route path={PATHS.SETTINGS} element={<Settings />} />
+                        <Route path={PATHS.ACCOUNT} element={<Account />} />
+                    </Route>
+
                     <Route path={PATHS.LOGIN} element={<Login />} />
                     <Route path={PATHS.PAGE_NOT_FOUND} element={<PageNotFound />} />
                 </Routes>
