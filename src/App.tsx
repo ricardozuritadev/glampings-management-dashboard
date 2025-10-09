5b40e0d2ba075649833ce9d349a4f902f72e30ac
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-09 10:34:28 -0500

    feat: modify app component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import { PATHS } from "./constants/paths";
+import { PATHS } from "./constants/paths.constants";
 
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Cabins";
+import Glampings from "./pages/Glampings";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
@@ -21,7 +21,7 @@ export default function App() {
                     <Route index element={<Navigate to={PATHS.DASHBOARD} />} />
                     <Route path={PATHS.DASHBOARD} element={<Dashboard />} />
                     <Route path={PATHS.BOOKINGS} element={<Bookings />} />
-                    <Route path={PATHS.CABINS} element={<Cabins />} />
+                    <Route path={PATHS.CABINS} element={<Glampings />} />
                     <Route path={PATHS.USERS} element={<Users />} />
                     <Route path={PATHS.SETTINGS} element={<Settings />} />
                     <Route path={PATHS.ACCOUNT} element={<Account />} />
