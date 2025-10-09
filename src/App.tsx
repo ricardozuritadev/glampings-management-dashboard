1cc4551fa9735b312f8f4040ecd1b927af55e6a0
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-09 09:57:48 -0500

    feat: implement app routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,34 @@
-import GlobalStyles from "./styles/GlobalStyles";
-import Heading from "@/ui/Heading";
-import Row from "./ui/Row";
-import Button from "./ui/Button";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-function App() {
+import { PATHS } from "./constants/paths";
+
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
+export default function App() {
     return (
         <>
             <GlobalStyles />
-            <Row>
-                <Heading>Hello World</Heading>
-                <Button size="large" variation="danger">
-                    Test 1
-                </Button>
-            </Row>
-
-            <Row type="vertical">
-                <Button size="small" variation="secondary">
-                    Test 2
-                </Button>
-                <Button>Test 3</Button>
-            </Row>
+            <BrowserRouter>
+                <Routes>
+                    <Route index element={<Navigate to={PATHS.DASHBOARD} />} />
+                    <Route path={PATHS.DASHBOARD} element={<Dashboard />} />
+                    <Route path={PATHS.BOOKINGS} element={<Bookings />} />
+                    <Route path={PATHS.CABINS} element={<Cabins />} />
+                    <Route path={PATHS.USERS} element={<Users />} />
+                    <Route path={PATHS.SETTINGS} element={<Settings />} />
+                    <Route path={PATHS.ACCOUNT} element={<Account />} />
+                    <Route path={PATHS.LOGIN} element={<Login />} />
+                    <Route path={PATHS.PAGE_NOT_FOUND} element={<PageNotFound />} />
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
-
-export default App;
