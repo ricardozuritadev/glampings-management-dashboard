5aebaa2e1043484f960cea7382df0cb99c1554d6
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-09 09:56:51 -0500

    feat: create paths constants.

---
+++ src/constants/paths.ts
@@ -0,0 +1,10 @@
+export const PATHS = {
+    DASHBOARD: "dashboard",
+    BOOKINGS: "bookings",
+    CABINS: "cabins",
+    USERS: "users",
+    SETTINGS: "settings",
+    ACCOUNT: "account",
+    LOGIN: "login",
+    PAGE_NOT_FOUND: "*"
+};
