d13c3dcd98fe3c561bf785e77b430fbcb0fcd226
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-12 10:42:43 -0500

    chore: install tanstack query and implement basic config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { PATHS } from "./constants/paths.constants";
 
@@ -12,11 +13,23 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000
+        }
+    }
+});
 
 export default function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
+
             <GlobalStyles />
+
             <BrowserRouter>
                 <Routes>
                     <Route element={<AppLayout />}>
@@ -33,6 +46,6 @@ export default function App() {
                     <Route path={PATHS.PAGE_NOT_FOUND} element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
-        </>
+        </QueryClientProvider>
     );
 }
