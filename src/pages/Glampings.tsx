3de3dcaee4e38e3f0243736f7db378b57d6c7790
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-13 16:35:46 -0500

    feat: modify glampings component.

--- src/pages/Glampings.tsx
+++ src/pages/Glampings.tsx
@@ -1,13 +1,20 @@
 import { PAGES } from "@/constants/pages.constants";
+import GlampingTable from "@/features/glampings/GlampingTable";
 
 import Heading from "@/ui/Heading";
 import Row from "@/ui/Row";
 
 export default function Glampings() {
     return (
-        <Row type="horizontal">
-            <Heading as="h1">{PAGES.GLAMPINGS.HEADER}</Heading>
-            <p>TEST</p>
-        </Row>
+        <>
+            <Row type="horizontal">
+                <Heading as="h1">{PAGES.GLAMPINGS.HEADER}</Heading>
+                <p>Filtrar / ordenar</p>
+            </Row>
+
+            <Row>
+                <GlampingTable />
+            </Row>
+        </>
     );
 }
