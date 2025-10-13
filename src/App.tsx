a8a972ab3f338aaae051a036e828277fec5a8fbd
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-13 16:28:18 -0500

    feat: modify stale time

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { PATHS } from "./constants/paths.constants";
 
@@ -13,12 +14,11 @@ import Settings from "./pages/Settings";
 import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            staleTime: 60 * 1000
+            staleTime: 0
         }
     }
 });
