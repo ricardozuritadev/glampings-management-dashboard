178db313f6b3e4d59509536c522c5fb47bce2740
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-10-22 17:05:05 -0500

    feat: create new ui form components

---
+++ src/ui/Form.tsx
@@ -0,0 +1,37 @@
+import type { FormHTMLAttributes, ReactNode } from "react";
+import styled, { css } from "styled-components";
+
+const StyledForm = styled.form<FormProps>`
+    ${(props) =>
+        props.type !== "modal" &&
+        css`
+            padding: 2.4rem 4rem;
+
+            /* Box */
+            background-color: var(--color-grey-0);
+            border: 1px solid var(--color-grey-100);
+            border-radius: var(--border-radius-md);
+        `}
+
+    ${(props) =>
+        props.type === "modal" &&
+        css`
+            width: 80rem;
+        `}
+    
+  overflow: hidden;
+    font-size: 1.4rem;
+`;
+
+type FormProps = FormHTMLAttributes<HTMLFormElement> & {
+    type?: "modal" | "regular";
+    children: ReactNode;
+};
+
+export default function Form({ type = "regular", children, ...props }: FormProps) {
+    return (
+        <StyledForm type={type} {...props}>
+            {children}
+        </StyledForm>
+    );
+}
