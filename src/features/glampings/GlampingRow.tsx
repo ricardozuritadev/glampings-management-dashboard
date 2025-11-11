bb8b3ec0e1a61dfbbd62b976058e3a9b5a8ca315
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-11-11 12:52:30 -0500

    feat: delete unused import

--- src/features/glampings/GlampingRow.tsx
+++ src/features/glampings/GlampingRow.tsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-import { useState } from "react";
 import { useDeleteGlamping } from "./useDeleteGlamping";
 
 import { formatCurrency, pluralize } from "@/utils/helpers";
