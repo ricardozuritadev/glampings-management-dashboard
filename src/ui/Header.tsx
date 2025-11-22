09dd8923688a799f79380fb4a38b953756f73086
Ricardo Zurita <ricardo.zurita.dev@gmail.com>
2025-11-22 07:53:18 -0500

    feat: build app header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,7 +1,13 @@
-import Logout from "@/features/auth/Logout";
 import styled from "styled-components";
 
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "@/features/auth/UserAvatar";
+
 const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    gap: 2rem;
     background-color: var(--color-grey-0);
     padding: 1.2rem 4.8rem;
     border-bottom: 1px solid var(--color-grey-100);
@@ -10,7 +16,8 @@ const StyledHeader = styled.header`
 export default function Header() {
     return (
         <StyledHeader>
-            <Logout />
+            <UserAvatar />
+            <HeaderMenu />
         </StyledHeader>
     );
 }
---
+++ src/ui/HeaderMenu.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+
+import { PATHS } from "@/constants/paths.constants";
+
+import ButtonIcon from "./ButtonIcon";
+import Logout from "@/features/auth/Logout";
+
+const StyledHeaderMenu = styled.ul`
+    display: flex;
+    gap: 0.4rem;
+`;
+
+export default function HeaderMenu() {
+    const navigate = useNavigate();
+
+    return (
+        <StyledHeaderMenu>
+            <li>
+                <ButtonIcon onClick={() => navigate(`/${PATHS.ACCOUNT}`)}>
+                    <HiOutlineUser />
+                </ButtonIcon>
+            </li>
+
+            <li>
+                <Logout />
+            </li>
+        </StyledHeaderMenu>
+    );
+}
